7fb3ae73e6c3358d1ecb6741f86d3593f3e4d927
Juliana Souza <julianaha01@gmail.com>
2022-08-23 18:21:16 -0300

    feat: inicializa  arquivos form em pages (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import FormStep1 from './pages/FormStep1/index';
+import FormStep2 from './pages/FormStep2/index';
+import FormStep3 from './pages/FormStep3/index';
 
 function App() {
-  return (
+  return (<>
    <FormStep1/>
+   <FormStep2/>
+   <FormStep3/>
+  </>
   )
 }
 
