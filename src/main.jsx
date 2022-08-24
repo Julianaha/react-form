e8e51f86bd18d24b0d71ebbb8895e842e862f25a
Juliana Souza <julianaha01@gmail.com>
2022-08-24 18:33:57 -0300

    Routes and forms (#2)
    * feat: inicializa  arquivos form em pages
    
    * feat: instala e importa dependencias de rota

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FormStep1 from "./pages/FormStep1";
+import FormStep2 from "./pages/FormStep2";
+import FormStep3 from "./pages/FormStep3";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<FormStep1 />} />
+        <Route path="/FormStep2" element={<FormStep2 />} />
+        <Route path="/FormStep3" element={<FormStep3 />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
