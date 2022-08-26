c66b97d60981fb60a02e81d312483169e9c6ed2f
Juliana <julianaha01@gmail.com>
2022-08-26 12:43:12 -0300

    feat: adiciona segundo formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import "./style/App.css";
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 import Navbar from "./components/Header";
 import FormStep1 from "./pages/FormStep1";
+import FormStep2 from "./pages/FormStep2";
 import { GiHamburgerMenu } from "@react-icons/all-files/gi/GiHamburgerMenu";
 
 function App() {
@@ -9,13 +11,18 @@ function App() {
 
   return (
     <>
-      <header>
-        <GiHamburgerMenu onClick={() => setShowNav(!showNav)} />
-      </header>
-      <Navbar show={showNav} />
-      <div className="main">
-        <FormStep1 />
-      </div>
+      <BrowserRouter>
+        <header>
+          <GiHamburgerMenu onClick={() => setShowNav(!showNav)} />
+        </header>
+        <Navbar show={showNav} />
+        <div className="main">
+          <Routes>
+            <Route path="/" expect element={<FormStep1 />} />
+            <Route path="/FormStep2" element={<FormStep2 />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/FormStep2/index.jsx
+++ src/pages/FormStep2/index.jsx
@@ -1,18 +1,47 @@
+import styles from "./FormStep2.module.css";
 export default function FormStep2() {
   return (
-    <form>
-      <label>
-        CEP
-        <input type="number" name="cep" value="" />
-      </label>
-      <label>
-        Endereço 1
-        <input type="text" name="adress1" />
-      </label>
-      <label>
-      Endereço 2
-        <input type="text" name="adress2" />
-      </label>
+    <form className={styles.form2}>
+      <fieldset className={styles.field2}>
+        <legend>Cadastro</legend>
+        <div class={styles.container2}>
+          <div className={styles.formGroup2}>
+            <label htmlFor="cep">CEP:</label>
+            <input
+              type="text"
+              id="cep"
+              className={styles.inputCep}
+              placeholder="00000-0000"
+            />
+          </div>
+          <div className={styles.adress}>
+            <div className={styles.formGroup2}>
+              <label htmlFor="adress1">Endereço 1:</label>
+              <input
+                type="text"
+                id="adress1"
+                className={styles.input1}
+                placeholder="Digite seu endereço"
+              />
+            </div>
+            <div className={styles.formGroup2}>
+              <label htmlFor="adress2">Endereço 2:</label>
+              <input
+                type="text"
+                id="adress2"
+                className={styles.input2}
+                placeholder="Digite um segundo endereço"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="">
+          <button type="submit" className="">
+            Próximo
+          </button>
+        </div>
+      </fieldset>
     </form>
   );
 }
