54c536d3a444ed73f7097fd26d98dc132f8e5d1c
Juliana <julianaha01@gmail.com>
2022-08-28 15:54:20 -0300

    refactor: context formulario

--- src/contexts/FormContext.jsx
+++ src/contexts/FormContext.jsx
@@ -1,12 +1,7 @@
-// Context, Reducer, Provider, Hook, useReducer
-import { createContext, useContext, useState } from "react";
+import { createContext } from "react";
 
-// Context
-const FormContext = createContext(state, dispatch);
-
-const initialData = {
-  currentStep: "",
-  name: "juliana",
+export const initialData = {
+  name: "",
   email: "",
   phone: "",
   cep: "",
@@ -17,62 +12,135 @@ const initialData = {
   money: "",
 };
 
+// Context
+export const UserContext = createContext(initialData);
+export const UserProvider = UserContext.Provider;
+
 //Reducer
-export const FormActions = {
-  setCurrentStep(){},
-  setName() {},
-  setLastName() {},
-  setEmail() {},
-  setPhone() {},
-  setCep() {},
-  setAdress1() {},
-  setAdress2() {},
-  setBirthDate() {},
-  setCpf() {},
-  setMoney() {},
+const SET_NAME = "SET_NAME";
+const SET_LASTNAME = "SET_LASTNAME";
+const SET_EMAIL = "SET_EMAIL";
+const SET_PHONE = "SET_PHONE";
+const SET_CEP = "SET_CEP";
+const SET_ADRESS1 = "SET_ADRESS1";
+const SET_ADRESS2 = "SET_ADRESS2";
+const SET_BIRTHDATE = "SET_BIRTHDATE";
+const SET_CPF = "SET_CPF";
+const SET_MONEY = "SET_MONEY";
+
+export const UserActions = {
+  setName(name) {
+    return {
+      type: SET_NAME,
+      payload: {
+        name: name,
+      },
+    };
+  },
+  setLastName(lastname) {
+    return {
+      type: SET_LASTNAME,
+      payload: {
+        lastname: lastname,
+      },
+    };
+  },
+  setEmail(email) {
+    return {
+      type: SET_EMAIL,
+      payload: {
+        email: email,
+      },
+    };
+  },
+  setPhone(phone) {
+    return {
+      type: SET_PHONE,
+      payload: {
+        phone: phone,
+      },
+    };
+  },
+  setCep(cep) {
+    return {
+      type: SET_CEP,
+      payload: {
+        cep: cep,
+      },
+    };
+  },
+  setAdress1(adress1) {
+    return {
+      type: SET_ADRESS1,
+      payload: {
+        adress1: adress1,
+      },
+    };
+  },
+  setAdress2(adress2) {
+    return {
+      type: SET_ADRESS2,
+      payload: {
+        adress2: adress2,
+      },
+    };
+  },
+  setBirthDate(birthdate) {
+    return {
+      type: SET_BIRTHDATE,
+      payload: {
+        birthdate: birthdate,
+      },
+    };
+  },
+  setCpf(cpf) {
+    return {
+      type: SET_CPF,
+      payload: {
+        cpf: cpf,
+      },
+    };
+  },
+  setMoney(money) {
+    return {
+      type: SET_MONEY,
+      payload: {
+        money: money,
+      },
+    };
+  },
 };
 
-const formReducer = (state, action) => {
+export const UserReducer = (state, action) => {
   switch (action.type) {
-    case FormActions.setCurrentStep:
-      return { ...state, currentStep: action.payload };
-    case FormActions.setName:
-      return { ...state, name: action.payload };
-    case FormActions.setLastName:
-      return { ...state, lastname: action.payload };
-    case FormActions.setEmail:
-      return { ...state, email: action.payload };
-    case FormActions.setPhone:
-      return { ...state, phone: action.payload };
-    case FormActions.setCep:
-      return { ...state, cep: action.payload };
-    case FormActions.setAdress1:
-      return { ...state, adress1: action.payload };
-    case FormActions.setAdress2:
-      return { ...state, adress2: action.payload };
-    case FormActions.setBirthDate:
-      return { ...state, birthdate: action.payload };
-    case FormActions.setCpf:
-      return { ...state, cpf: action.payload };
-    case FormActions.setMoney:
-      return { ...state, money: action.payload };
+    case SET_NAME:
+      return { ...state, name: action.payload.name };
+    case SET_LASTNAME:
+      return { ...state, lastname: action.payload.lastname };
+    case SET_EMAIL:
+      return { ...state, email: action.payload.email };
+    case SET_PHONE:
+      return { ...state, phone: action.payload.phone };
+    case SET_CEP:
+      return { ...state, cep: action.payload.cep };
+    case SET_ADRESS1:
+      return { ...state, adress1: action.payload.adress1 };
+    case SET_ADRESS2:
+      return { ...state, adress2: action.payload.adress2 };
+    case SET_BIRTHDATE:
+      return { ...state, birthdate: action.payload.birthdate };
+    case SET_CPF:
+      return { ...state, cpf: action.payload.cpf };
+    case SET_MONEY:
+      return { ...state, money: action.payload.money };
     default:
       return state;
   }
 };
 
 // Provider
-export const FormProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(formReducer, initialData);
-  const value = { state, dispatch };
-  return <FormContext.Provider value={value}>{children}</FormContext.Provider>;
-};
-
-// Context Hook
-export const useForm = () => {
-  const context = useContext(FormContext);
-  if (context === undefined) {
-    throw new Error("useForm precisa ser usado dentro do FormProvider");
-  }
-  return context;
-};
+// export const FormProvider = ({ children }) => {
+//   const [state, dispatch] = useReducer(formReducer, initialData);
+//   const value = { state, dispatch };
+//   return <FormContext.Provider value={value}>{children}</FormContext.Provider>;
+// };
