b8205b14d4cf866fa679e4cced878d3e51ef7cba
Juliana <julianaha01@gmail.com>
2022-08-28 17:09:18 -0300

    refactor: armazena navbar em componente

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,27 @@
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import { MdPersonAdd, MdGroups } from "react-icons/md";
+
+const Navbar = ({ show }) => {
+  return (
+    <div className={show ? "sidenav && active" : "sidenav"}>
+      <img src={logo} width="80px" alt="logo" />
+      <ul>
+        <li>
+          <a href="/Clientes">
+            <MdGroups width="80px"/>
+            Clientes
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <MdPersonAdd />
+            Cadastro
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
