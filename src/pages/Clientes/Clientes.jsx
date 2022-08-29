390dc5b65f2a71a80c4b8f14c88bcec0a018475c
Juliana <julianaha01@gmail.com>
2022-08-28 21:22:11 -0300

    feat: cria tabela com dados do cliente

--- src/pages/Clientes/Clientes.jsx
+++ src/pages/Clientes/Clientes.jsx
@@ -1,5 +1,48 @@
-export default function Clientes () {
-    return(
-        <h1>Clientes</h1>
-    )
+import { useContext } from "react";
+import { ClientContext } from "../../contexts/ClientsContext";
+import Table from "react-bootstrap/Table";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Clientes() {
+  const [clients, clientDispatch] = useContext(ClientContext);
+
+  return (
+    <>
+      <p></p>
+      <Table striped bordered hover responsive="lg">
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Sobrenome</th>
+            <th>Email</th>
+            <th>Telefone</th>
+            <th>CEP</th>
+            <th>Endereço1</th>
+            <th>Endereço2</th>
+            <th>Data Nascimento</th>
+            <th>CPF</th>
+            <th>Renda Mensal</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clients.map((client, index) => {
+            return (
+              <tr key={index}>
+                <td>{client.name}</td>
+                <td>{client.lastname}</td>
+                <td>{client.email}</td>
+                <td>{client.phone}</td>
+                <td>{client.cep}</td>
+                <td>{client.adress1}</td>
+                <td>{client.adress2}</td>
+                <td>{client.birthdate}</td>
+                <td>{client.cpf}</td>
+                <td>{client.money}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </>
+  );
 }
