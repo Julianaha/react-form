88a9b93be9a26ba5df0bf5453fff2c9d6211ad88
Juliana <julianaha01@gmail.com>
2022-08-28 21:20:38 -0300

    refactor: adiciona dados mockados do cliente

--- src/contexts/ClientsContext.jsx
+++ src/contexts/ClientsContext.jsx
@@ -1,6 +1,31 @@
 import { createContext } from "react";
 
-const initialData = [];
+export const initialData = [
+  {
+    name: "Juliana",
+    lastname: "Souza",
+    email: "julianaha01@gmail.com",
+    phone: "12 992033219",
+    cep: "88495-000",
+    adress1: "30 de dezembro",
+    adress2: "Major Pedro",
+    birthdate: "01/08/1994",
+    cpf: "466.343.231-34",
+    money: "1000",
+  },
+  {
+    name: "Juliana",
+    lastname: "Mota",
+    email: "julianaha01@gmail.com",
+    phone: "12 992033219",
+    cep: "88495-000",
+    adress1: "30 de dezembro",
+    adress2: "Major Pedro",
+    birthdate: "01/08/1994",
+    cpf: "466.343.231-34",
+    money: "1000",
+  },
+];
 export const ClientContext = createContext(initialData);
 export const ClientProvider = ClientContext.Provider;
 
@@ -17,7 +42,7 @@ export const ClientActions = {
 export const ClientReducer = (state, action) => {
   switch (action.type) {
     case SET_ADDCLIENT:
-      return { ...state, user: action.payload.user };
+      return [...state, action.payload];
     default:
       return state;
   }
